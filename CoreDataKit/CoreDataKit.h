eb949a1a7a0816a718f145282cd13c8baa427ea4
Bogdan Filippov <bvfilippov@gmail.com>
2023-09-09 11:41:16 +0700

    Initial Commit

---
+++ CoreDataKit/CoreDataKit.h
@@ -0,0 +1,18 @@
+//
+//  CoreDataKit.h
+//  CoreDataKit
+//
+//  Created by Bogdan Filippov on 9/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreDataKit.
+FOUNDATION_EXPORT double CoreDataKitVersionNumber;
+
+//! Project version string for CoreDataKit.
+FOUNDATION_EXPORT const unsigned char CoreDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataKit/PublicHeader.h>
+
+
